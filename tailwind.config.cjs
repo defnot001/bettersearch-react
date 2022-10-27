6650b8e54bb198e918514dddb23862877ebf82a1
defnot001 <marc.miller994@gmail.com>
2022-10-27 23:35:35 +0200

    added custom font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,9 @@ module.exports = {
   mode: 'jit',
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      poppins: ['Poppins', 'sans-serif'],
+    },
   },
   plugins: [],
 };
