2db3f26503c485fd1938dd2498fecd9293faee8b
defnot001 <marc.miller994@gmail.com>
2022-10-27 23:24:14 +0200

    initial commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
 function App() {
   return (
-    <div className="flex h-screen w-screen bg-neutral-900 text-gray-100"></div>
+    <div className="flex h-screen w-screen bg-neutral-900 text-gray-100">
+      <section>Bettersearch</section>
+    </div>
   );
 }
 
